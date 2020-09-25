a0642a43b55b0d27e440dacb9cd6b346b49a7042
LH <lh@lhdv.net>
2020-09-24 21:39:46 -0300

    Create Pages Controller
    - Add Home method/view
    - Add About method/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  get 'pages/about'
+  root 'pages#home'
+
+  get 'about', to: 'pages#about'
 end
