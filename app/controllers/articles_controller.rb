67585b4f3af6deec22449e8b91b0878f7749c842
LH <lh@lhdv.net>
2020-09-26 18:47:52 -0300

    Refactor controller and views to DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    # This line was moved to a private method to DRY
+    # @article = Article.find(params[:id])
+
   end
 
   def new
@@ -18,7 +22,7 @@ class ArticlesController < ApplicationController
     # Can't perform the following assigment due strong parameters validation
     # @article = Article.new(params[:article])
 
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article
     if @article.save
       
@@ -36,7 +40,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # This line was moved to a private method to DRY
+    # @article = Article.find(params[:id])
+
   end
 
   def update
@@ -50,8 +56,10 @@ class ArticlesController < ApplicationController
     # if Article.update(params.require(:article).permit(:title, :description))
     # if Article.update(params.require(:article).permit(:id, :title, :description))
     
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))    
+    # This line was moved to a private method to DRY
+    # @article = Article.find(params[:id])
+
+    if @article.update(article_params)    
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -61,8 +69,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    # This line was moved to a private method to DRY
+    # @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
