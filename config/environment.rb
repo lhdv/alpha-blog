72f186283a99f1c1c4d33e90981392fb2a86aab6
LH <lh@lhdv.net>
2020-09-28 17:51:15 -0300

    Refactor styling in Articles
    - Create a shared errors partial to be reusable across other views
    - Change config/environment.rb to disable issue the <div class="field_with_errors">
    - Change form/new/edit views

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#
+# Remove field_with_erros div when ActiveRecord returns a validation error
+#
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
