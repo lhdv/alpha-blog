823d4ead73ec915c80c3eed168a06118ffa1ff94
LH <lh@lhdv.net>
2020-09-27 21:32:20 -0300

    Style Home page with bootstrap
    - Add Jumbotron
    - Add sticky footer

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpacker = require("webpack")
+
+environment.plugins.append("Provide", new webpacker.ProvidePlugin({
+        $:'jquery',
+        jQuery: 'jquery',
+        Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
