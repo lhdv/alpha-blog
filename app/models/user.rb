21698f52b69983854ff33aa2a8956abc5fd06e03
LH <lh@lhdv.net>
2020-09-28 21:29:01 -0300

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  
+  validates :username, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :username, length: { in: 3..25 }
+
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, length: { maximum: 105 }
+  validate :validates_user_email
+
+  private
+
+  def validates_user_email
+    unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      errors.add(:email, "is not an email")
+    end
+  end
+
 end
