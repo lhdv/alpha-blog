4e00321a14fe74b95bdb01361869743393b44b34
LH <lh@lhdv.net>
2020-09-29 21:30:45 -0300

    Add User#create controller action
    - Refactor existing reusable code to work with new User model changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    # byebug
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You've signed up to Alpha Blog #{@user.username}!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
