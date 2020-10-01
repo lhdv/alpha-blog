4b6c08c9f8ca669184a519ecc990988bec7bcf8d
LH <lh@lhdv.net>
2020-09-30 21:42:21 -0300

    Add Sessions controller
    - Create routes
    - Create a new Controller
    - Create Sessions#new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Rails.application.routes.draw do
+  #
+  # Pages routes
+  #
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  #
+  # Articles routes
+  #
   # The line below is the same as the above
   # resources :articles, only: [:show, :index, :create, :new, :edit, :update, :destroy]
   resources :articles
 
+  #
+  # Users routes
+  #
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
+
+  #
+  # Sessions routes
+  #
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
