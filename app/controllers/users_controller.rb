a08310c743242a434f6d4162a46c1d5d7afbbfdb
LH <lh@lhdv.net>
2020-09-30 21:21:42 -0300

    Add pagination to Articles and Users
    - Add will_paginate gem
    - Paginate Article#index, User#index and User#show when showing its articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
