a909c119bae5022014b38ea926f9d68c3e0db9b1
LH <lh@lhdv.net>
2020-10-01 20:40:23 -0300

    Create SessionsController to control logged users
    - Create logic to control a user login/logout
    - Save the user on a controller session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,32 @@ class SessionsController < ApplicationController
   end
   
   def create
+    @user = User.find_by(email: session_params[:email].downcase).try(:authenticate, session_params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome back #{@user.username}"
+      redirect_to @user
+    else
+      # When you render a partial there's no HTTP request made so
+      # the flash[:notice] could not happen.
+      # When you trigger a flash.now the message is immediatelly presented
+      # flash[:notice] = "Incorrect email and/or password"
+      flash.now[:alert] = "Incorrect email and/or password"
+      render 'new'
+    end
   end
   
   def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      flash[:notice] = "Logged out! See you later!"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
