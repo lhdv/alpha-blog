adf73b10e0b7ea71d91361eb2e12cc082eaf32d8
LH <lh@lhdv.net>
2020-10-02 18:48:23 -0300

    Restrict User change
    - Only allow an user be changed or destroyed by itself(and logged in)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -46,5 +47,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit/delete your own profile"
+      redirect_to @user
+    end
   end
 end
