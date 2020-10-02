ccd462d80975aaa8e64558fb1f7b2d83f8c71364
LH <lh@lhdv.net>
2020-10-02 20:38:58 -0300

    Allow admin User delete other Users and change flash messages style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
-    @current_user = nil
+    if @user == current_user
+      session[:user_id] = nil
+      @current_user = nil
+    end
     
     @user.destroy    
 
@@ -60,7 +62,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit/delete your own profile"
       redirect_to @user
     end
