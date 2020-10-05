132f1de3bac218c49aa5f6663c83a0ca516492b2
LH <lh@lhdv.net>
2020-10-05 19:48:42 -0300

    Create a test and implementation for Category#new/create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,10 +7,28 @@ class CategoriesController < ApplicationController
   def new
     # To make the test pass a render could solve or just create the html.erb of the action
     # render html: 'new'
+
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created!"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def show
     # To make the test pass a render could solve or just create the html.erb of the action
     # render html: 'show'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
